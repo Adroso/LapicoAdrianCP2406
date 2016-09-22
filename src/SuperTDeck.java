1d8886cadc398df4ff1d95839a549a3ae408b329
Adroso <adrian.lapico@my.jcu.edu.au>
2016-09-22 22:36:05 +1000

    added deck import1

--- src/SuperTDeck.java
+++ src/SuperTDeck.java
@@ -1,4 +1,11 @@
 import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.*;
+
+import jdk.nashorn.internal.parser.JSONParser;
+import org.json.*;
 /**
  * Created by Adroso360 on 3/09/2016.
  */
@@ -6,7 +13,36 @@ public class SuperTDeck {
     private static final int NUM_INIT_CARDS = 60;
     private ArrayList<SuperTCard> cards;
 
-    // import the XML card file
+    public static void ImportCards(){
+        // Reading the JSON file of cards
+        JSONParser parser = new JSONParser();
+
+        try {
+        Object obj = parser.parse(new FileReader("c:\\file.json"));
+
+        JSONObject jsonObject =  (JSONObject) obj;
+
+        String name = (String) jsonObject.get("name");
+        System.out.println(name);
+
+        String city = (String) jsonObject.get("city");
+        System.out.println(city);
+
+        String job = (String) jsonObject.get("job");
+        System.out.println(job);
+
+        // loop array
+        JSONArray cars = (JSONArray) jsonObject.get("cars");
+        Iterator<String> iterator = cars.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+        } catch (FileNotFoundException e) {
+        e.printStackTrace();
+        } catch (IOException e) {
+         // import the XML card file
+     }
+
     public SuperTDeck(){
         cards = new ArrayList<SuperTCard>();
         for (int i = 0; i < NUM_INIT_CARDS; i++) {
