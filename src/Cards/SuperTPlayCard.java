a15e75c296dd599d7170b0678be41c16788ea04d
Adroso <adrian.lapico@my.jcu.edu.au>
2016-09-30 01:18:30 +1000

    Added ALL Card Object Instances (Still needs a little formatting)

--- src/Cards/SuperTPlayCard.java
+++ src/Cards/SuperTPlayCard.java
@@ -1,5 +1,6 @@
 package Cards;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
@@ -9,15 +10,29 @@ public class SuperTPlayCard extends BaseSuperTCard {
     protected String chemistry;
     protected String classification;
     protected String crysSystem;
-    protected ArrayList occurrence;
-    protected String hardness;
-    protected String specGravity;
+    protected String occurrence;
+    protected double lowhardness;
+    protected double highhardness;
+    protected double lowspecGravity;
+    protected double highspecGravity;
     protected String cleavage;
     protected String crustAbundance;
     protected String ecoVal;
 
-    public SuperTPlayCard (String fileName, String imageName, String title, ) {
+    public SuperTPlayCard (String fileName, String imageName, String title, String chemistry, String classification, String crysSystem, String occurrence, double lowhardness, double highhardness, double lowspecGravity, double highspecGravity, String cleavage, String crustAbundance, String ecoVal) {
         super(fileName, imageName, title);
+        this.chemistry = chemistry;
+        this.classification = classification;
+        this.crysSystem = crysSystem;
+        this.occurrence = occurrence;
+        this.lowhardness = lowhardness;
+        this.highhardness = highhardness;
+        this.lowspecGravity = lowspecGravity;
+        this.highspecGravity = highspecGravity;
+        this.cleavage = cleavage;
+        this.crustAbundance = crustAbundance;
+        this.ecoVal = ecoVal;
+
 
     }
 
