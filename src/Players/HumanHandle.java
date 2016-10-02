94178f7a031b4b38994b9934398732e5c3350631
Adroso360 <adrian.lapico@my.jcu.edu.au>
2016-10-02 10:34:55 +1000

    Fixed bug where Input checking would not display specific message

--- src/Players/HumanHandle.java
+++ src/Players/HumanHandle.java
@@ -15,21 +15,20 @@ public class HumanHandle {
     public String getCategory(String categories) {
         String[] localCats = categories.split(", ");
         //handles if there is only 1 category passed in.
-        if (localCats.length == 1){
+        if (localCats.length == 1) {
             return localCats[0];
         }
-        for (int i = 0; i < localCats.length; i++){
-            System.out.println(i + " : " + localCats[i] );
+        for (int i = 0; i < localCats.length; i++) {
+            System.out.println(i + " : " + localCats[i]);
         }
         int userInput = -1;
-        while (userInput < 0 || userInput > localCats.length-1) {
+        while (userInput < 0 || userInput > localCats.length - 1) {
             try {
                 System.out.println("Enter Your choice: ");
                 userInput = new Scanner(System.in).nextInt();
-            }
-            catch (InputMismatchException p2) {
+            } catch (InputMismatchException p2) {
                 System.out.println("Please Enter Valid Number Input");
-            } catch (IndexOutOfBoundsException p6){
+            } catch (IndexOutOfBoundsException p6) {
                 System.out.println("There is no Category at: " + userInput);
             }
         }
@@ -38,8 +37,8 @@ public class HumanHandle {
 
     public Card getCard(Card currentCard, String currentCat, Player currentPlayer) {
         //handles if there is only 1 category passed in.
-        for (int i = 0; i < currentPlayer.hand.size(); i++){
-            System.out.println(i + " : " + currentPlayer.hand.get(i).title );
+        for (int i = 0; i < currentPlayer.hand.size(); i++) {
+            System.out.println(i + " : " + currentPlayer.hand.get(i).title);
         }
         System.out.println(currentPlayer.hand.size() + " : Don't Play A Card");
         int userInput = -1;
@@ -48,23 +47,22 @@ public class HumanHandle {
                 System.out.println("Enter Your choice: ");
                 userInput = new Scanner(System.in).nextInt();
 
-                if(userInput == currentPlayer.hand.size()){
+                if (userInput == currentPlayer.hand.size()) {
                     return currentCard;
                 }
             } catch (InputMismatchException p2) {
                 System.out.println("Please Enter Valid Number Input");
-            } catch (IndexOutOfBoundsException p5){
-                System.out.println("There is no card at:" + userInput);
             }
-            if (userInput > 0 || userInput < currentPlayer.hand.size())
-                if (currentCard instanceof PlayCard && !currentPlayer.hand.get(userInput).isBetterThan((PlayCard) currentCard,currentCat)){
+            if (userInput > 0 && userInput < currentPlayer.hand.size()) {
+                if (currentCard instanceof PlayCard && !currentPlayer.hand.get(userInput).isBetterThan((PlayCard) currentCard, currentCat)) {
                     System.out.println(currentPlayer.hand.get(userInput).title + " is not better than " + currentCard.title);
                     userInput = -1;
-                }
-                else
-                    if (currentCard instanceof TrumpCard){
+                } else if (currentCard instanceof TrumpCard) {
                     return currentPlayer.hand.get(userInput);
-                    }
+                }
+            } else {
+                System.out.println("Please choose a valid option"); // handles if user inputs greater than List
+            }
         }
 
         return currentPlayer.hand.get(userInput);
