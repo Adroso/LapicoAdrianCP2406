9986bf92429896aba5c3f820011b767f0a936f4b
Adroso <adrian.lapico@my.jcu.edu.au>
2016-10-05 20:51:39 +1000

    Began Constructing GUI for Part 2

---
+++ src/GUI/GUITest.java
@@ -0,0 +1,15 @@
+package GUI;
+
+/**
+ * Created by Adroso360 on 5/10/2016.
+ */
+
+import javax.swing.*;
+public class GUITest {
+    public static void main(String[] args) {
+        JFrame mainFrame = new JFrame("Mineral Super Trumps! - The Computer Game");
+        mainFrame.setSize(1400, 900);
+        mainFrame.setVisible(true);
+        mainFrame.setDefaultLookAndFeelDecorated(true);
+    }
+}
