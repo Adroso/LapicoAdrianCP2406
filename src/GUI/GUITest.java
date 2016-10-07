ec36aa2022935c394c34a8f1dd37e6d30d83b5ed
Adroso360 <adrian.lapico@my.jcu.edu.au>
2016-10-07 12:45:12 +1000

    Small Edits to GUI setup

--- src/GUI/GUITest.java
+++ src/GUI/GUITest.java
@@ -5,11 +5,15 @@ package GUI;
  */
 
 import javax.swing.*;
+import java.awt.*;
+
 public class GUITest {
     public static void main(String[] args) {
-        JFrame mainFrame = new JFrame("Mineral Super Trumps! - The Computer Game");
-        mainFrame.setSize(1400, 900);
+        JFrame mainFrame = new JFrame("Mineral Super Trumps! - The Computer Game!");
+        Font mainFont = new Font("Arial", Font.ITALIC, 35);
+        mainFrame.setSize(1100, 700);
         mainFrame.setVisible(true);
         mainFrame.setDefaultLookAndFeelDecorated(true);
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
